162cf7faa9520621b9bd081a6ac973b2f4fce64c
James <jamesholton2511@gmail.com>
2023-03-31 03:45:27 +1100

    All web SC and jQuery

---
+++ 09-James-Holton/wk02/2-tue/Computer-fansite/page1/js/fansite.js
@@ -0,0 +1,10 @@
+// const button = document.getElementById('square-button');
+
+// const square = function() {
+
+//   const square = document.getElementById('square-input').value;
+//   alert(square * square);
+
+// };
+
+// button.addEventListener('click', square);
