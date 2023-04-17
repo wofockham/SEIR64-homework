60715e3731912267933efa3e8a7112fc7a2aeb12
James <jamesholton2511@gmail.com>
2023-04-18 09:35:20 +1000

    unfinished

---
+++ 09-James-Holton/wk05/1-mon/create_companies.sql
@@ -0,0 +1,8 @@
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    image TEXT
+);
+
+INSERT INTO companies (name) VALUES ('Volkswagen');
+INSERT INTO companies (name) VALUES ('Toyota');
